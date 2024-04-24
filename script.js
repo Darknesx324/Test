ff4227bc5754a573f7111eaa32408e10eff63723
monitor sistemas <monitor_sistemas@fadp.edu.co>
2024-04-24 14:51:50 -0500

    Se sube el proyecto al repositorios

---
+++ script.js
@@ -0,0 +1,34 @@
+function sumar() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("result").innerText = "Resultado: " + resultado;
+}
+
+function restar() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 - num2;
+    document.getElementById("result").innerText = "Resultado: " + resultado;
+}
+
+function multiplicar() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 * num2;
+    document.getElementById("result").innerText = "Resultado: " + resultado;
+}
+
+function dividir() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    if (num2 === 0) {
+        document.getElementById("result").innerText = "No se puede dividir por cero";
+    } else {
+        var resultado = num1 / num2;
+        document.getElementById("result").innerText = "Resultado: " + resultado;
+    }
+
+
+
+}
